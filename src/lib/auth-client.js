940210397c336786c945b78479268c2b989ee400
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 22:39:51 +0600

    add

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.MY_UI
 })
 
 export const { signIn, signUp, signOut , useSession } = createAuthClient()
