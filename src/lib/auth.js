759f526a1dad0248ae5f8f5d55ff5a73ce515791
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 19:27:27 +0600

    add

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,15 @@
 import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MY_UI);
+const db = client.db("next-Auth");
 
 export const auth = betterAuth({
-  //...
+    emailAndPassword: { 
+        enabled: true, 
+    },
+    database: mongodbAdapter(db, {
+        client
+    }),
 });
