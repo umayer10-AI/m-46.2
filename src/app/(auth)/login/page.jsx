7d52df950ec7195e7e51fe66a1702d44424bbc7a
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 18:27:46 +0600

    add

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,25 +1,37 @@
+"use client"
 import Navbar from '@/component/Navbar';
+import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
+
+    const a = (e) => {
+        e.preventDefault()
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        console.log(email,password)
+    }
+
     return (
         <div>
             <Navbar></Navbar>
             <div className='flex justify-center mt-5 mb-10'>
-                <form className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
+                <form onSubmit={a} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
 
                     <h2 className='text-3xl font-semibold text-center mb-5'>Register your account</h2>
 
                     <label className="label text-black font-semibold text-base">Email address</label>
-                    <input type="email" className="input w-full" placeholder="Enter email address" />
+                    <input type="email" name='email' className="input w-full" placeholder="Enter email address" />
 
                     <label className="label text-black font-semibold text-base">Password</label>
-                    <input type="password" className="input w-full" placeholder="Enter your password" />
+                    <input type="password" name='password' className="input w-full" placeholder="Enter your password" />
 
-                    <button className="btn btn-neutral my-4">Login</button>
+                    <button type='submit' className="btn btn-neutral my-4">Login</button>
 
-                    <p className='text-center font-semibold text-base'>Dont’t Have An Account ? <span className='
-                    text-red-600'>Register</span></p>
+                    <p className='text-center font-semibold text-base'>Dont’t Have An Account ? <Link href={'/register'} className='
+                    text-red-600'>Register</Link></p>
                 </form>
             </div>
         </div>
