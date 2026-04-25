44089686dcfccddc1fc5e5ed2f38b80025453d7c
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 19:02:41 +0600

    add

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,31 +2,35 @@
 import Navbar from '@/component/Navbar';
 import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const page = () => {
 
-    const a = (e) => {
-        e.preventDefault()
+    const {register,handleSubmit,watch,formState: {errors}} = useForm()
 
-        const email = e.target.email.value
-        const password = e.target.password.value
-
-        console.log(email,password)
+    const onSubmit = (data) => {
+        console.log(data)
     }
+
+    console.log(watch("email"))
+    console.log(watch("password"))
+    
 
     return (
         <div>
             <Navbar></Navbar>
             <div className='flex justify-center mt-5 mb-10'>
-                <form onSubmit={a} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
+                <form onSubmit={handleSubmit(onSubmit)} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
 
                     <h2 className='text-3xl font-semibold text-center mb-5'>Register your account</h2>
 
                     <label className="label text-black font-semibold text-base">Email address</label>
-                    <input type="email" name='email' className="input w-full" placeholder="Enter email address" />
+                    <input type="email" {...register("email", { required: "email Filed required" })} className="input w-full" placeholder="Enter email address" />
+                    {errors.email && <p className='text-red-600 font-semibold'>{errors.email.message} </p>}
 
                     <label className="label text-black font-semibold text-base">Password</label>
-                    <input type="password" name='password' className="input w-full" placeholder="Enter your password" />
+                    <input type="password" {...register("password", { required: "password Filed required" })} className="input w-full" placeholder="Enter your password" />
+                    {errors.password && <p className='text-red-600 font-semibold'> {errors.password.message}</p>}
 
                     <button type='submit' className="btn btn-neutral my-4">Login</button>
 
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,26 +1,42 @@
+"use client"
 import Navbar from '@/component/Navbar';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const page = () => {
+
+    const {register,handleSubmit,watch,formState: {errors}} = useForm()
+    
+        const onSubmit = (data) => {
+            console.log(data)
+        }
+    
+        // console.log(watch("email"))
+        // console.log(watch("password"))
+
     return (
         <div>
             <Navbar></Navbar>
             <div className='flex justify-center mt-5 mb-10'>
-                <form className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
+                <form onSubmit={handleSubmit(onSubmit)} className="fieldset bg-base-200 border-base-300 rounded-box w-md border p-8">
 
                     <h2 className='text-3xl font-semibold text-center mb-5'>Register your account</h2>
 
                     <label className="label text-black font-semibold text-base">Your Name</label>
-                    <input type="text" className="input w-full" placeholder="Enter your name" />
+                    <input type="text" {...register("name", { required: "name Filed required" })} className="input w-full" placeholder="Enter your name" />
+                    {errors.name && <p className='text-red-600 font-semibold'>{errors.name.message} </p>}
 
                     <label className="label text-black font-semibold text-base">Photo URL</label>
-                    <input type="text" className="input w-full" placeholder="Enter your url" />
+                    <input type="text" {...register("url", { required: "url Filed required" })} className="input w-full" placeholder="Enter your url" />
+                    {errors.url && <p className='text-red-600 font-semibold'>{errors.url.message} </p>}
 
-                    <label className="label text-black font-semibold text-base">Email</label>
-                    <input type="email" className="input w-full" placeholder="Email" />
+                    <label className="label text-black font-semibold text-base">Email address</label>
+                    <input type="email" {...register("email", { required: "email Filed required" })} className="input w-full" placeholder="Enter email address" />
+                    {errors.email && <p className='text-red-600 font-semibold'>{errors.email.message} </p>}
 
                     <label className="label text-black font-semibold text-base">Password</label>
-                    <input type="password" className="input w-full" placeholder="Password" />
+                    <input type="password" {...register("password", { required: "password Filed required" })} className="input w-full" placeholder="Enter your password" />
+                    {errors.password && <p className='text-red-600 font-semibold'> {errors.password.message}</p>}
 
                     <label className="label text-black mt-2 font-semibold"><input type="checkbox" className="checkbox checkbox-info" />Accept Term & Conditions</label>
 
